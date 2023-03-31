59759f4658d5a0547308003787f922d2274f37db
Cococry <cococry.btw@gmail.com>
2023-03-31 19:37:50 +0200

    String Literal fundamental support

--- lantern.c
+++ lantern.c
@@ -17,8 +17,16 @@
     }                                                                                   \
 }                                                                                       \
 
+/*
+ * "Hello World" <- Pushed to the heap
+ *               <- Pointer to it is stored on the stack
+ *               -> [printstr] interprete the value on the stack as a pointer to memory and cast it to char*
+ *  
+ *
+ * */
+
 typedef enum {
-    INST_STACK_PUSH, INST_STACK_PREV,
+    INST_STACK_PUSH, INST_STACK_PREV, INST_STACK_PUSH_HEAP_PTR,
     INST_PLUS, INST_MINUS, INST_MUL, INST_DIV,
     INST_EQ,INST_NEQ, 
     INST_GT,INST_LT,INST_GEQ,INST_LEQ,
@@ -39,7 +47,7 @@ typedef enum {
 
 typedef struct {
     Instruction inst;
-    int32_t data;
+    size_t data;
 } Token;
 
 typedef struct {
@@ -47,6 +55,9 @@ typedef struct {
     int32_t stack_size;
 
     uint32_t inst_ptr;
+
+    void** heap;
+    uint32_t heap_size;
 } ProgramState;
 
 bool is_str_int(const char* str) {
@@ -55,17 +66,18 @@ bool is_str_int(const char* str) {
     }
     return true;
 }
-Token* load_program_from_file(const char* filepath, uint32_t* program_size) {
-    FILE* file_count_words;
-    file_count_words = fopen(filepath, "r");
-    if(!file_count_words) {
+Token* load_program_from_file(const char* filepath, uint32_t* program_size, ProgramState* state) {
+    (void)state;
+    FILE* file;
+    file = fopen(filepath, "r");
+    if(!file) {
         printf("Lantern: [Error]: Cannot read file '%s'.\n", filepath);
         return NULL;
     }
-    uint32_t words_in_file = 1;
+    uint32_t words_in_file = 0;
     char ch;
     bool on_word = false;
-    while ((ch = fgetc(file_count_words)) != EOF) {
+    while ((ch = fgetc(file)) != EOF) {
         if(ch == ' ' || ch == '\t' || ch == '\0' || ch == '\n') {
             if (on_word) {
                 on_word = false;
@@ -75,17 +87,82 @@ Token* load_program_from_file(const char* filepath, uint32_t* program_size) {
             on_word = true;
         }
     }
-    fclose(file_count_words);
 
+    rewind(file);    
+    char line[256];
+    char** literals = malloc(256 * (*program_size));
+    bool removed_word_from_literal = false;
+    uint32_t literal_count = 0;
+    while (fgets(line, sizeof(line), file)) {
+        char literal[256] = "";
+        bool on_literal = false;
+        uint32_t literal_char_count = 0;
+        for(uint32_t i = 0; i < strlen(line); i++) {
+            if(line[i] == '"' && !on_literal) {
+                on_literal = true;
+                literal[literal_char_count++] = line[i];
+                continue;
+            }
+            if(on_literal) {
+                literal[literal_char_count++] = line[i];
+                if(line[i] == ' ' && !removed_word_from_literal) {
+                    removed_word_from_literal = true;
+                    words_in_file--;
+                } else if(line[i] != ' ') {
+                    removed_word_from_literal = false;
+                }
+            }
+            if(line[i] == '"' && on_literal) {
+                on_literal = false;
+                literal[literal_char_count] = '\0';
+                literal_char_count = 0;
+                char* literal_cpy = malloc(256);
+                strncpy(literal_cpy, literal, 256);
+                literals[literal_count++] = literal_cpy;
+            }
+        }
+    }
     *program_size = words_in_file;
-    
-    FILE* file;
-    file = fopen(filepath, "r");
+    rewind(file);
 
     Token* program = malloc(sizeof(Token) * words_in_file);
     char word[128];
     uint32_t i = 0;
+    bool on_literal_token = false;
+    uint32_t literal_token_count = 0;
     while(fscanf(file, "%s", word) != EOF) {
+        if(!on_literal_token && word[0] == '"' && word[strlen(word) - 1] == '"' && strlen(word) > 1) {
+            on_literal_token = false;
+            char* literal_cpy = malloc(256);
+            strcpy(literal_cpy, literals[literal_token_count]);
+            state->heap[state->heap_size] = literal_cpy;
+            program[i] = (Token){ .inst = INST_STACK_PUSH_HEAP_PTR, .data = state->heap_size};
+            state->heap_size++;
+            printf("Literal at %i: %s\n", literal_token_count, literals[literal_token_count]);
+            literal_token_count++;
+            printf("hi\n");
+            i++;
+            continue;
+        }
+        if(!on_literal_token && word[0] == '"') {
+            on_literal_token = true;
+            char* literal_cpy = malloc(256);
+            strcpy(literal_cpy, literals[literal_token_count]);
+            state->heap[state->heap_size] = literal_cpy;
+            program[i] = (Token){ .inst = INST_STACK_PUSH_HEAP_PTR, .data = state->heap_size};
+            state->heap_size++;
+            printf("Literal at %i: %s\n", literal_token_count, literals[literal_token_count]); 
+            literal_token_count++;
+            i++;
+            continue;
+        }
+        if(on_literal_token && word[strlen(word) - 1] == '"') {
+            on_literal_token = false;
+            continue;
+        }
+        if(on_literal_token) {
+            continue;
+        }
         if(is_str_int(word)) {
             program[i] = (Token){ .inst = INST_STACK_PUSH, .data = atoi(word) };
             i++;
@@ -134,11 +211,20 @@ Token* load_program_from_file(const char* filepath, uint32_t* program_size) {
         }
         i++;
     }
-    fclose(file);  
+    fclose(file); 
 
+    for(uint32_t i = 0; i < state->heap_size; i++) {
+        printf("Heap: %s\n", (char*)state->heap[i]);
+    }
+
+    free(literals);
     return program;
 }
 
+void heap_free(ProgramState* state, uint32_t pos) {
+    free(state->heap[pos]);
+    state->heap_size--;
+}
 void exec_program(ProgramState* state, Token* program, uint32_t program_size) {
     for(uint32_t i = 0; i < program_size; i++) {
         if(program[i].inst == INST_ELSE) {
@@ -157,7 +243,7 @@ void exec_program(ProgramState* state, Token* program, uint32_t program_size) {
                     break;
                 }
             }
-            PANIC_ON_ERR(program[i].data == -1, ERR_SYNTAX_ERROR, "While-end token without while condition.");
+            PANIC_ON_ERR(program[i].data == (size_t)-1, ERR_SYNTAX_ERROR, "While-end token without while condition.");
         }
         if(program[i].inst == INST_RUN_WHILE) {
             for(uint32_t j = i; j < program_size; j++) {
@@ -166,7 +252,7 @@ void exec_program(ProgramState* state, Token* program, uint32_t program_size) {
                     break;
                 }
             }
-            PANIC_ON_ERR(program[i].data == -1, ERR_SYNTAX_ERROR, "While condtion without end token.");
+            PANIC_ON_ERR(program[i].data == (size_t)-1, ERR_SYNTAX_ERROR, "While condtion without end token.");
         }
         if(program[i].inst == INST_IF) {
             program[i].data = -1;
@@ -176,7 +262,7 @@ void exec_program(ProgramState* state, Token* program, uint32_t program_size) {
                     break;
                 }
             }
-            if(program[i].data == -1) {
+            if(program[i].data == (size_t)-1) {
                 for(uint32_t k = i; k < program_size; k++) {
                     if(program[k].inst == INST_ENDIF) {
                         program[i].data = k;
@@ -184,10 +270,11 @@ void exec_program(ProgramState* state, Token* program, uint32_t program_size) {
                     }
                 }
             }
-            PANIC_ON_ERR(program[i].data == -1, ERR_SYNTAX_ERROR, "If without endif");
+            PANIC_ON_ERR(program[i].data == (size_t)-1, ERR_SYNTAX_ERROR, "If without endif");
         }
     }
     while(state->inst_ptr < program_size) {
+        //printf("Instruction: %i\n", (int)program[state->inst_ptr].inst);
         if(program[state->inst_ptr].inst == INST_RUN_WHILE) {
             PANIC_ON_ERR(state->stack_size < 1, ERR_STACK_UNDERFLOW, "No value for while condition specified.");
             int32_t cond = state->stack[state->stack_size - 1];
@@ -203,7 +290,7 @@ void exec_program(ProgramState* state, Token* program, uint32_t program_size) {
             Token tok = program[state->inst_ptr];
             state->inst_ptr = tok.data;
         }
-        if (program[state->inst_ptr].inst == INST_STACK_PUSH) {
+        if (program[state->inst_ptr].inst == INST_STACK_PUSH || program[state->inst_ptr].inst == INST_STACK_PUSH_HEAP_PTR) {
             PANIC_ON_ERR(state->stack_size >= STACK_CAP, ERR_STACK_OVERFLOW, "Stack is overflowed");
             state->stack[state->stack_size++] = program[state->inst_ptr].data;
         }
@@ -225,9 +312,16 @@ void exec_program(ProgramState* state, Token* program, uint32_t program_size) {
         }
         if(program[state->inst_ptr].inst == INST_PRINT) {
             PANIC_ON_ERR(state->stack_size < 1, ERR_STACK_UNDERFLOW, "No value for print function on stack.");
-            int32_t val = state->stack[state->stack_size - 1];
-            state->stack_size -= 1;
-            printf("%i\n", val);
+            if(program[state->inst_ptr - 1].inst == INST_STACK_PUSH) {
+                int32_t val = state->stack[state->stack_size - 1];
+                state->stack_size -= 1;
+                printf("%i\n", val);
+            } else if(program[state->inst_ptr - 1].inst == INST_STACK_PUSH_HEAP_PTR) {
+                char* val = state->heap[program[state->inst_ptr - 1].data];
+                state->stack_size -= 1;
+                printf("%s\n", val);
+                heap_free(state, program[state->inst_ptr - 1].data);
+            }
         }
         if(program[state->inst_ptr].inst == INST_JUMP) {
             int32_t index = program[state->inst_ptr].data;
@@ -309,11 +403,14 @@ int main(int argc, char** argv) {
         return 1;
     }
     uint32_t program_size;
-    Token* program = load_program_from_file(argv[1], &program_size);
     ProgramState program_state;
     program_state.stack_size = 0;
+    program_state.heap_size = 0;
     program_state.inst_ptr = 0;
+    program_state.heap = malloc(1024 /* TODO: Dynamic Heap */);
+    Token* program = load_program_from_file(argv[1], &program_size, &program_state);
 
     exec_program(&program_state, program, program_size);
+    free(program_state.heap);
     return 0;
 }
